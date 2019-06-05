a4a069e088b52da8f89ccb0d5219fe43cd69d9a5
lbwa <mr_lbw@outlook.com>
2019-06-05 18:03:48 +0800

    fix(functions): correct http response handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,13 +6,13 @@ import express = require('express')
 
 exports.getRequestOrigin = functions.https.onRequest(
   /**
-   * @description
+   * @description Same as express response handler
    * https://firebase.google.com/docs/reference/functions/functions.https?authuser=0#.onRequest
    */
   async (req: express.Request, res: express.Response) => {
     if (req.method !== 'GET') res.sendStatus(403)
-    const remoteClientIp =
-      req.headers['x-forwarded-for'] || req.connection.remoteAddress
-    res.status(200).send(remoteClientIp)
+    res.send({
+      origin: req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    })
   }
 )
