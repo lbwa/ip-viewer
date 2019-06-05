6e99820a1aa5d70bd304ce9209451a619e8a70be
lbwa <mr_lbw@outlook.com>
2019-06-06 00:20:48 +0800

    feat(frontend): detail process

--- src/request/index.ts
+++ src/request/index.ts
@@ -2,7 +2,7 @@ import http from './axios'
 
 export async function detectFromGoogle() {
   // edit DETECT_FORM_XXX variables from .env.local
-  // const { data } = await http.get(process.env.REACT_APP_DETECT_FROM_GOOGLE as string)
-  const { data } = await http.get('https://api.github.com')
+  const { data } = await http.get(process.env
+    .REACT_APP_DETECT_FROM_GOOGLE as string)
   return data
 }
