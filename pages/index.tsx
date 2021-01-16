a272c993d3ced7e777d197c30dffef33aae66f48
Liu Bowen <mr_lbw@outlook.com>
2021-01-16 12:23:33 +0800

    refactor: use rxjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,65 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import { css } from '@emotion/css'
-import axios from 'axios'
+import { ajax } from 'rxjs/ajax'
+import { map } from 'rxjs/operators'
+
+enum Url {
+  LOCAL = 'https://myip.ipip.net', // 'https://api64.ipify.org?format=json'
+  OVERSEA = 'https://us-central1-ip-viewer-6.cloudfunctions.net/getRequestOrigin',
+}
 
 const Home: React.FC<HomeProps> = function Home() {
-  const [worldWide, setWorldWide] = useState('loading')
+  const [overseaState, setOverseaState] = useState('loading')
   const [localState, setLocalState] = useState('loading')
 
   useEffect(() => {
-    axios
-      .get<string>('https://myip.ipip.net')
-      .then(({ data }) => setLocalState(data))
-      .catch(() => setLocalState('failed'))
-
-    axios
-      .get<{ origin: string }>(
-        'https://us-central1-ip-viewer-6.cloudfunctions.net/getRequestOrigin'
+    ajax({
+      url: Url.LOCAL,
+      responseType: 'text',
+    })
+      .pipe(map((r) => r.response))
+      .subscribe(
+        (res) => setLocalState(res),
+        () => setLocalState(`Failed`)
       )
-      .then(({ data }) => setWorldWide(data.origin))
-      .catch(() => setWorldWide('failed'))
+
+    ajax(Url.OVERSEA)
+      .pipe(map((r) => r.response))
+      .subscribe(
+        (res) => setOverseaState(res.origin),
+        () => setOverseaState(`Failed`)
+      )
+
+    // zip(
+    //   of({ url: Url.LOCAL, responseType: 'text' }, Url.OVERSEA).pipe(
+    //     // mergeMap 将不保证 subscriber 中收到的结果顺序
+    //     mergeMap((opt) => ajax(opt))
+    //   ),
+    //   of(setLocalState, setOverseaState)
+    // ).subscribe(([{ response }, setter]) =>
+    //   setter(typeof response === 'string' ? response : response.ip)
+    // )
+
+    // zip(
+    //   of({ url: Url.LOCAL, responseType: 'text' }, Url.OVERSEA).pipe(
+    //     // concatMap 虽能保证结果顺序，但将导致按序列请求，而非并发
+    //     concatMap((opt) =>
+    //       ajax(opt).pipe(
+    //         map(({ response }) =>
+    //           typeof response === 'object' ? response.ip : response
+    //         ),
+    //         map((res) => (console.log(res), res))
+    //       )
+    //     ),
+    //     materialize()
+    //   ),
+    //   of(setLocalState, setOverseaState)
+    // ).subscribe(([{ hasValue, value }, setter]) => {
+    //   hasValue && setter(value)
+    //   console.log('hasValue, value', hasValue, value)
+    // })
+
     return () => {}
   }, [])
   return (
@@ -36,15 +77,15 @@ const Home: React.FC<HomeProps> = function Home() {
         className={css`
           margin: 0 auto;
           display: flex;
-          flex-direction: column;
-          justify-content: center;
+          justify-content: space-around;
           align-items: center;
+          flex-wrap: wrap;
           height: calc(100vh - 52px);
         `}
       >
         {[
-          ['world wide', worldWide],
-          ['local', localState],
+          ['Oversea state', overseaState],
+          ['Local state', localState],
         ].map(([title, state]) => (
           <div
             className={css`
